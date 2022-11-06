cd6055d93c4110da285aa14cdecc80dd1ad5f261
EvgeniyRuban <84354312+EvgeniyRuban@users.noreply.github.com>
2022-11-06 22:56:43 +0600

    Added LibraryService.Web.

---
+++ LibraryService.Web/Models/BookViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LibraryServiceReference;
+
+namespace LibraryService.Web.Models;
+
+public class BookViewModel
+{
+    public Book[] Books { get; set; }
+    public SearchType SearchType { get; set; }
+    public string? SearchString { get; set; }
+}
