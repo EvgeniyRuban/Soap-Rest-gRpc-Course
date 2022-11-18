58ea95de9b01ae876194f6259d598315b385133e
EvgeniyRuban <eugene_1997@inbox.ru>
2022-11-19 03:55:20 +0600

    Refactoring

--- ClinicService.Api/Program.cs
+++ ClinicService.Api/Program.cs
@@ -1,4 +1,4 @@
-using ClinicService.BusinessLogic.Services;
+using ClinicService.Api.Services;
 using ClinicService.DAL;
 using ClinicService.DAL.Repos;
 using ClinicService.Domain.Repos;
@@ -6,46 +6,45 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
 
-namespace ClinicService.Api
+namespace ClinicService.Api;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddGrpc();
+
+        builder.WebHost.ConfigureKestrel(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddGrpc();
-
-            builder.WebHost.ConfigureKestrel(options =>
+            options.Listen(IPAddress.Any, 5001, listenOptions =>
             {
-                options.Listen(IPAddress.Any, 5001, listenOptions =>
-                {
-                    listenOptions.Protocols = HttpProtocols.Http2;
-                });
+                listenOptions.Protocols = HttpProtocols.Http2;
             });
+        });
 
-            builder.Services.AddDbContext<ClinicServiceDbContext>(options =>
-            {
-                options.UseSqlServer(builder.Configuration["DatabaseSettings:ConnectionString"]);
-            });
+        builder.Services.AddDbContext<ClinicServiceDbContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration["DatabaseSettings:ConnectionString"]);
+        });
 
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
 
-            builder.Services.AddScoped<IClientRepository, ClientRepository>();
+        builder.Services.AddScoped<IClientRepository, ClientRepository>();
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            app.MapControllers();
+        app.MapControllers();
 
-            app.UseRouting();
+        app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGrpcService<ClientService>();
-            });
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGrpcService<ClientService>();
+        });
 
-            app.Run();
-        }
+        app.Run();
     }
 }
